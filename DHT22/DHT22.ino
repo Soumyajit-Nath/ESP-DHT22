0d348163de54fdb15a5bf56bf17f696a5a24bbe9
Soumyajit-Nath <138509031+Soumyajit-Nath@users.noreply.github.com>
2024-10-10 00:45:11 +0530

    Add files via upload

---
+++ DHT22/DHT22.ino
@@ -0,0 +1,32 @@
+#include<Arduino.h>
+#include<Wire.h>
+#include <DHT.h>
+
+#define DHTPIN 4 
+#define DHTTYPE DHT22  
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float humi = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float temp = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  
+  // Check if any reads failed and exit early (to try again).
+  
+
+  
+  Serial.print(F("Humidity: "));
+  Serial.print(humi);
+  Serial.println(F("%  Temperature: "));
+  Serial.print(temp);
+  Serial.print(F("°C "));
+  Serial.println();
+  delay(3000);
+}
